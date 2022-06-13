0534c8bd9ee0b3c3c9f640c616a28d3f4ef802e0
Fffiestochka <Fffiestochka@mail.ru>
2022-06-13 21:38:53 +0300

    feat: add lang switch and local storage

---
+++ translate.js
@@ -0,0 +1,105 @@
+const translate = {
+  'eng': {
+    'nav-contacts': 'Contacts',
+    'nav-about': 'About me',
+    'nav-skills': 'Skills',
+    'nav-code': 'Code',
+    'nav-projects': 'Projects',
+    'nav-experience': 'Experience',
+    'nav-video': 'Video',
+    'nav-education': 'Education',
+    'main-title': 'Vera Leonova',
+    'section-title-contacts': 'Contacts',
+    'my-location': 'Nizhny Novgorod, Russia',
+    'section-title-about': 'About me',
+    'about-me-text':
+      "I'm looking to become a junior front-end developer. My goal is to develop professionally and gain competencies in order to be useful in solving team and personal tasks.",
+    'section-title-skills': 'Skills',
+    'section-title-code': 'Code',
+    'section-title-video': 'Video-presentation',
+    'section-title-projects': 'Projects',
+    'project-portfolio': 'Portfolio',
+    'project-shelter': 'Shelter',
+    'project-keyboard': 'Keyboard',
+    'project-slider': 'CSS slider',
+    'section-title-experience': 'Experience',
+    'alfa-organization': 'Alfa-Service LLC (2019-now)',
+    'alfa-position': 'Logistic coordinator:',
+    'alfa-duties': 'electronics import and export coordination.',
+    'niro-organization':
+      'Nizhny Novgorod Institute of the Education Development (2018-2019)',
+    'niro-position':
+      'Chief specialist of the regional branch server support department:',
+    'niro-duties':
+      'consulting, organizational, methodological and technical activities to support the functioning of educational software products.',
+    'kindergarten-organization': 'Kindergarten № 477 (2012-2018)',
+    'kindergarten-position': 'Educator (first qualification category):',
+    'kindergarten-duties':
+      'planning and organization of educational activities.',
+    'ds-organization': 'RPC Diagnostic Systems, Ltd (2002-2012)',
+    'ds-position': 'Customs Team Engineer, then Leader:',
+    'ds-duties':
+      'organization and coordination of customs clearance of export and import cargo.',
+    'section-title-education': 'Education',
+    'nngu-organization':
+      'Lobachevsky State University of Nizhny Novgorod (1997-2002)',
+    'nngu-speciality':
+      'Faculty of Biology, Animal and Human Physiology and Biochemistry Department',
+    'ngpu-organization':
+      'Kozma Minin Nizhny Novgorod State Pedagogical University (2012-2015)',
+    'ngpu-speciality':
+      'Specialty "Pre-school pedagogy and psychology", qualification "Teacher of pre-school pedagogy and psychology", specialization "Development and correction of speech"',
+  },
+  'rus': {
+    'nav-contacts': 'Контакты',
+    'nav-about': 'Обо мне',
+    'nav-skills': 'Навыки',
+    'nav-code': 'Код',
+    'nav-projects': 'Проекты',
+    'nav-experience': 'Опыт работы',
+    'nav-video': 'Видео',
+    'nav-education': 'Образование',
+    'main-title': 'Вера Леонова',
+    'section-title-contacts': 'Контакты',
+    'my-location': 'Нижний Новгород, Россия',
+    'section-title-about': 'Обо мне',
+    'about-me-text':
+      'Я стремлюсь получить позицию junior front-end developer. Моя цель - профессионально развиваться и осваивать новые навыки, чтобы приносить пользу в решении командных и личных задач.',
+    'section-title-skills': 'Навыки',
+    'section-title-code': 'Код',
+    'section-title-video': 'Видео-презентация',
+    'section-title-projects': 'Проекты',
+    'project-portfolio': 'Портфолио',
+    'project-shelter': 'Приют',
+    'project-keyboard': 'Клавиатура',
+    'project-slider': 'CSS слайдер',
+    'section-title-experience': 'Опыт работы',
+    'alfa-organization': 'ООО Альфа Сервис (2019-н.в.)',
+    'alfa-position': 'Логист координатор:',
+    'alfa-duties': 'координация импорта и экспорта электроники.',
+    'niro-organization':
+      'Нижегородский Институт Развития Образования (2018-2019)',
+    'niro-position':
+      'Главный специалист отдела обеспечения регионального отраслевого сервера:',
+    'niro-duties':
+      'консультационная, организационно-методическая, организационно-техническая деятельность по сопровождению функционирования программных продуктов, установленных на региональном отраслевом сервере.',
+    'kindergarten-organization': 'Детский сад №477 (2012-2018)',
+    'kindergarten-position': 'Воспитатель (первая квалификационная категория):',
+    'kindergarten-duties':
+      'планирование и организация учебно-воспитательной деятельности.',
+    'ds-organization': 'ООО НПО «Диагностические системы» (2002-2012)',
+    'ds-position':
+      'Инженер в отделе таможенного оформления, затем начальник отдела:',
+    'ds-duties':
+      'организация и координация таможенного оформления экспортных и импортных грузов.',
+    'section-title-education': 'Образование',
+    'nngu-organization': 'ННГУ им. Н.И.Лобачевского (1997-2002)',
+    'nngu-speciality':
+      'Биологический факультет, кафедра физиологии и биохимии животных и человека',
+    'ngpu-organization': 'НГПУ им. К.Минина (2012-2015)',
+    'ngpu-speciality':
+      'Специальность «Дошкольная педагогика и психология», квалификация «Преподаватель дошкольной педагогики и психологии», специализация «Развитие и коррекция речи»',
+  },
+};
+
+export default translate;
